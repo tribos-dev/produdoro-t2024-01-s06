5ee119f5b38203d1a540c4bfc7beb6a4e8ddb6c9
Bruno Mosaniel <brunomosaniel.dev@gmail.com>
2024-06-17 19:18:06 -0300

    Adiciona a classe edita tarefa Request

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/EditaTarefaRequest.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/EditaTarefaRequest.java
@@ -1,4 +1,16 @@
 package dev.wakandaacademy.produdoro.tarefa.application.api;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class EditaTarefaRequest {
+    @NotBlank
+    @Size(message = "Campo descrição tarefa não pode estar vazio", max = 255, min = 3)
+    private String descricao;
 }
