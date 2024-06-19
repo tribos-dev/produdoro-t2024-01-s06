e23d74a928b33fe56cd456b37a09c8a6163704f2
Patrick Melo <patrickabimael@yahoo.com.br>
2024-06-19 11:09:40 -0300

    implementa método que incrementa pomodoro na camada de service

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaService.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaService.java
@@ -5,7 +5,9 @@ import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaRequest;
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
 
 import java.util.UUID;
+
 public interface TarefaService {
     TarefaIdResponse criaNovaTarefa(TarefaRequest tarefaRequest);
     Tarefa detalhaTarefa(String usuario, UUID idTarefa);
+    void incrementaPomodoro(String emailDoUsuario, UUID idTarefa);
 }
